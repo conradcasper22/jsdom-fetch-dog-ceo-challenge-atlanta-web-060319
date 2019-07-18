97c9479d5e6f5c86aab2ed3b58658d3af1e825f2
github username <github email address>
2019-07-18 14:42:13 -0400

    almost done with the first challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', pageSetup)
+
+function pageSetup(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(json => addImgToDom(json))
+}
+
+function addImgToDom(json){
+    json.forEach(function(url){
+      const img =  document.createElement('img')
+      img.src = url
+
+    })
+}
